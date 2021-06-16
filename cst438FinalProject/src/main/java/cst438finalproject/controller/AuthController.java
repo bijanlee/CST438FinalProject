e16b3e7ac4d45ce27ac8e6c9a9b026296afcef3a
Salasc2 <salasch2@gmail.com>
2021-06-16 14:47:23 -0700

    add user login, logout and signup feature

--- cst438FinalProject/src/main/java/cst438finalproject/controller/AuthController.java
+++ cst438FinalProject/src/main/java/cst438finalproject/controller/AuthController.java
@@ -3,9 +3,20 @@ package cst438finalproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import cst438finalproject.domain.User;
+import cst438finalproject.domain.UserRepository;
 
 @Controller
 public class AuthController {
+    @Autowired
+    private UserRepository repo;
 
    @GetMapping("/login")
    public String Login(Model model) {
@@ -14,7 +25,27 @@ public class AuthController {
    
    @GetMapping("/register")
    public String Register(Model model) {
+      model.addAttribute("user", new User());
       return "register";
+   }
+   
+   @PostMapping("/success_register")
+   public String successRegister(User user) {
+       BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+       String encodedPassword = passwordEncoder.encode(user.getPasswd());
+       user.setPasswd(encodedPassword);
+        
+       repo.save(user);
+        
+       return "success_register";
+   }
+   
+   @GetMapping("/users")
+   public String listUsers(Model model) {
+       List<User> listUsers = repo.findAll();
+       model.addAttribute("listUsers", listUsers);
+        
+       return "users";
    }
    
 }
